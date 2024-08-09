4a49ed1de3c87a7df287123c5c238ac19d8c3c2e
Mikhail <mdruchek@yandex.ru>
2024-08-09 09:08:28 +0300

    updated models.py

--- blogs_app/models.py
+++ blogs_app/models.py
@@ -3,24 +3,23 @@ from typing_extensions import Annotated
 from sqlalchemy import Integer, String, Sequence
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 
-str50 = Annotated[str, 50]
-str500 = Annotated[str, 500]
+
+required_str = Annotated[str, mapped_column(String, nullable=False)]
+required_str50 = Annotated[str, mapped_column(String(50), nullable=False)]
+required_str500 = Annotated[str, mapped_column(String(500), nullable=False)]
 
 
 class Base(DeclarativeBase):
-    type_annotation_map = {
-        str50: String(50),
-        str500: String(500)
-    }
+    pass
 
 
 class User(Base):
     """Модель User"""
     __tablename__ = 'user'
 
-    id: Mapped[int] = mapped_column(Integer, Sequence('user_id_seq'), primary_key=True)
-    name: Mapped[str50]
-    api_key: Mapped[str50]
+    id: Mapped[int] = mapped_column(Integer, Sequence('user_id_seq'))
+    name: Mapped[required_str50]
+    api_key: Mapped[required_str50]
 
 
 class Tweet(Base):
@@ -28,7 +27,7 @@ class Tweet(Base):
     __tablename__ = 'tweet'
 
     id: Mapped[int] = mapped_column(Integer, Sequence('tweet_id_seq'), primary_key=True)
-    content: Mapped[str500]
+    content: Mapped[required_str500]
     author_id: Mapped[int]
 
 
@@ -47,7 +46,7 @@ class Media(Base):
 
     id: Mapped[int] = mapped_column(Integer, Sequence('media_id_seq'), primary_key=True)
     tweet_id: Mapped[int]
-    url: Mapped[str]
+    url: Mapped[required_str]
 
 
 class Follow(Base):
