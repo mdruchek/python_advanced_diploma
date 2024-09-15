7be9129aac0bb6bbf675ecbcf31fd25b4ad3cd00
Mikhail Druchek <mdruchek@yandex.ru>
2024-09-15 09:10:01 +0300

    added me route in routes/user.py

--- blogs_app/routes/user.py
+++ blogs_app/routes/user.py
@@ -1,4 +1,27 @@
-from flask import Blueprint
+from flask import Blueprint, request
 
 
 bp = Blueprint('users', __name__, url_prefix='/api/users')
+
+@bp.route('/me', methods=('GET',))
+def me():
+    api_kyi = request.args.get('api-key')
+    return {
+        "result": True,
+        "user": {
+            "id": 1,
+            "name": 'Незнайка',
+            "followers":[
+                {
+                    "id":"int",
+                    "name":"str"
+                }
+            ],
+            "following":[
+                {
+                    "id":"int",
+                    "name":"str"
+                }
+            ]
+        }
+    }
