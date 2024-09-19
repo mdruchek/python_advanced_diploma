a5b531c3ac97bdb740054da0d9dedb212c94f1ee
Druchek_MA <mdruchek@yandex.ru>
2024-09-19 14:46:31 +0300

    update Meta class UserFactory

--- blogs_app/factories.py
+++ blogs_app/factories.py
@@ -9,8 +9,8 @@ from blogs_app import database
 class UserFactory(factory.alchemy.SQLAlchemyModelFactory):
     class Meta:
         model = User
-        sqlalchemy_session = database.get_db()
-        sqlalchemy_get_or_create = ('api_key',)
+        sqlalchemy_session = database.Session()
+        sqlalchemy_session_persistence = "commit"
 
     name = factory.Faker('first_name', locale='ru_Ru')
     api_key = FuzzyText(length=5, chars=string.ascii_letters+string.digits)
