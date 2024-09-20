7c814eb1778c765eda4412e4f431ba39edd357b1
Druchek_MA <mdruchek@yandex.ru>
2024-09-20 15:17:31 +0300

    routes/user.py: updated /me route

--- blogs_app/routes/user.py
+++ blogs_app/routes/user.py
@@ -1,27 +1,35 @@
 from flask import Blueprint, request
+from sqlalchemy import select
+
+from blogs_app import database
+from blogs_app import models
+from blogs_app import schemas
 
 
 bp = Blueprint('users', __name__, url_prefix='/api/users')
 
 @bp.route('/me', methods=('GET',))
 def me():
-    api_kyi = request.args.get('api-key')
-    return {
-        "result": True,
-        "user": {
-            "id": 1,
-            "name": 'Незнайка',
-            "followers":[
-                {
-                    "id":"int",
-                    "name":"str"
-                }
-            ],
-            "following":[
-                {
-                    "id":"int",
-                    "name":"str"
-                }
-            ]
-        }
-    }
+    api_key = request.args.get('api-key')
+    db = database.get_db()
+    user = db.execute(select(models.User).where(models.User.api_key == api_key))
+    return user.mo
+    # {
+    #     "result": True,
+    #     "user": {
+    #         "id": 6,
+    #         "name": 'Кнопочка',
+    #         "followers":[
+    #             {
+    #                 "id":1,
+    #                 "name":"Семён"
+    #             }
+    #         ],
+    #         "following":[
+    #             {
+    #                 "id":2,
+    #                 "name":"Лавр"
+    #             }
+    #         ]
+    #     }
+    # }
