e1154e3e4aeb08d335211d5546e87f1774337470
Mikhail Druchek <mdruchek@yandex.ru>
2024-09-21 06:48:40 +0300

    schemas.py: rename schemas

--- blogs_app/schemas.py
+++ blogs_app/schemas.py
@@ -1,31 +1,31 @@
 from pydantic import BaseModel
 
 
-class User(BaseModel):
+class UserSchemas(BaseModel):
     id: int
     name: str
     apy_key: str
 
 
-class Tweet(BaseModel):
+class TweetSchemas(BaseModel):
     id: int
     content: str
     author_id: int
 
 
-class Like(BaseModel):
+class LikeSchemas(BaseModel):
     id: int
     user_id: int
     tweet_id: int
 
 
-class Media(BaseModel):
+class MediaSchemas(BaseModel):
     id: int
     tweet_id: int
     url: str
 
 
-class Follow(BaseModel):
+class FollowSchemas(BaseModel):
     id: int
     author_id: int
     follower_id: int
