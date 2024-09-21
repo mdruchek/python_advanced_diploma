d061a237cba7b2d3f9c16973c63e51aad522b064
Mikhail Druchek <mdruchek@yandex.ru>
2024-09-21 07:45:44 +0300

    schemas.py: fixed bug in the User Schema and renamed the schemas names

--- blogs_app/schemas.py
+++ blogs_app/schemas.py
@@ -1,31 +1,31 @@
 from pydantic import BaseModel
 
 
-class UserSchemas(BaseModel):
+class UserSchema(BaseModel):
     id: int
     name: str
-    apy_key: str
+    api_key: str
 
 
-class TweetSchemas(BaseModel):
+class TweetSchema(BaseModel):
     id: int
     content: str
     author_id: int
 
 
-class LikeSchemas(BaseModel):
+class LikeSchema(BaseModel):
     id: int
     user_id: int
     tweet_id: int
 
 
-class MediaSchemas(BaseModel):
+class MediaSchema(BaseModel):
     id: int
     tweet_id: int
     url: str
 
 
-class FollowSchemas(BaseModel):
+class FollowSchema(BaseModel):
     id: int
     author_id: int
     follower_id: int
