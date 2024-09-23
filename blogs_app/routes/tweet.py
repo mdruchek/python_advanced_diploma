1c7c8d2f6327390fa352144d10b8cc1fefa2d1e4
Druchek_MA <mdruchek@yandex.ru>
2024-09-23 15:19:39 +0300

    routes/tweet.p: created route create_tweet

--- blogs_app/routes/tweet.py
+++ blogs_app/routes/tweet.py
@@ -1,4 +1,14 @@
-from flask import Blueprint
+from flask import Blueprint, request
+
+from blogs_app import schemas
 
 
 bp = Blueprint('tweet', __name__, url_prefix='/api/tweets')
+
+
+@bp.route('/', methods=('POST',))
+def create_tweet():
+    api_key = request.headers.get('Api-Key')
+    request_data = request.json
+    print(request_data)
+    return ''
