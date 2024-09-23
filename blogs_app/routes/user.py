5a2584ba7d839fa19ee426b1831ef6c961965bcf
Druchek_MA <mdruchek@yandex.ru>
2024-09-23 15:20:54 +0300

    updated route /me

--- blogs_app/routes/user.py
+++ blogs_app/routes/user.py
@@ -10,28 +10,27 @@ bp = Blueprint('users', __name__, url_prefix='/api/users')
 
 @bp.route('/me', methods=('GET',))
 def me():
-    api_key = request.args.get('api-key')
+    api_key = request.headers.get('Api-Key')
     db = database.get_db()
     user = db.execute(select(models.User).where(models.User.api_key == api_key)).scalar()
-    user_dict = user.to_dict()
-    user = schemas.UserSchema(**user_dict)
-    return user.model_dump_json()
-# {
-#         "result": True,
-#         "user": {
-#             "id": 6,
-#             "name": 'Кнопочка',
-#             "followers":[
-#                 {
-#                     "id":1,
-#                     "name":"Семён"
-#                 }
-#             ],
-#             "following":[
-#                 {
-#                     "id":2,
-#                     "name":"Лавр"
-#                 }
-#             ]
-#         }
-#     }
+    # user_dict = user.to_dict()
+    # user = schemas.UserSchema(**user_dict)
+    return {#user.model_dump_json()
+        "result": True,
+        "user": {
+            "id": 6,
+            "name": 'Кнопочка',
+            "followers":[
+                {
+                    "id":1,
+                    "name":"Семён"
+                }
+            ],
+            "following":[
+                {
+                    "id":2,
+                    "name":"Лавр"
+                }
+            ]
+        }
+    }
