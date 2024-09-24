49d82e1de8ab1229836adf77d340f583882ad593
Druchek_MA <mdruchek@yandex.ru>
2024-09-24 15:22:25 +0300

    routes/tweet.py: added get_tweets route

--- blogs_app/routes/tweet.py
+++ blogs_app/routes/tweet.py
@@ -1,5 +1,9 @@
-from flask import Blueprint, request
+from flask import Blueprint, request, jsonify
 
+from sqlalchemy import select
+
+from blogs_app import database
+from blogs_app import models
 from blogs_app import schemas
 
 
@@ -12,3 +16,50 @@ def create_tweet():
     request_data = request.json
     print(request_data)
     return ''
+
+
+@bp.route('/', methods=('GET',))
+def get_tweets():
+    db = database.get_db()
+    tweets = db.execute(select(models.Tweet)).scalars().all()
+    tweets_list_of_dict = []
+    for tweet in tweets:
+        tweet_dict = tweet.to_dict()
+        tweet_dict.pop('author_id')
+        tweet_dict['author'] = tweet.author.to_dict(exclude=('api_key',))
+        tweet_dict['likes'] = [like.user.to_dict(exclude=('api_key',)) for like in tweet.likes]
+        tweets_list_of_dict.append(tweet_dict)
+    print(tweets_list_of_dict)
+    return jsonify(
+        {
+            'result': True,
+            'tweets': tweets_list_of_dict
+        }
+    )
+
+
+    # {
+    # “result”: true,
+    # "tweets": [
+    #     {
+    #         "id": int,
+    #         "content": string,
+    #         "attachments"[
+    #             link_1, // relative?
+    # link_2,
+    # ...
+    # ]
+    # "author": {
+    #     "id": int
+    #     "name": string
+    # }
+    # “likes”: [
+    #     {
+    # “user_id”: int,
+    # “name”: string
+    # }
+    # ]
+    # },
+    # ...,
+    # ]
+    # }
