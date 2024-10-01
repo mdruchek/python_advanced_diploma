219407569ca18cf0b35575694ae62ee3bd7d9745
Mikhail Druchek <mdruchek@yandex.ru>
2024-10-01 20:42:26 +0300

    routes/media.py: updated upload_medias route

--- blogs_app/routes/medias.py
+++ blogs_app/routes/medias.py
@@ -1,4 +1,5 @@
 import os
+from fileinput import filename
 
 from flask import Blueprint, request, current_app, jsonify
 from werkzeug.utils import secure_filename
@@ -12,13 +13,23 @@ bp = Blueprint('medias', __name__, url_prefix='/api/medias')
 @bp.route('/', methods=('POST',))
 def upload_medias():
     db = database.get_db()
+    api_key = request.headers.get('Api-Key')
     file = request.files['file']
     if file and allowed_file(file.filename):
         filename = secure_filename(file.filename)
-        file_url = os.path.join(current_app.instance_path ,current_app.config['UPLOAD_FOLDER'], filename)
-        file.save(file_url)
+        static_folder = os.path.join(current_app.instance_path ,current_app.config['UPLOAD_FOLDER'])
+        user_folder = os.path.join(static_folder, api_key)
+        if not os.path.exists(static_folder):
+            os.makedirs(static_folder)
+        if not os.path.exists(user_folder):
+            os.makedirs(user_folder)
+        file_path = os.path.exists(os.path.join(user_folder, filename))
+        if os.path.exists(file_path):
 
-        file_obj = models.Media(url=file_url)
+        file_path = os.path.join(user_folder, filename)
+        file.save(file_path)
+
+        file_obj = models.Media(url=file_path)
         db.add(file_obj)
         db.commit()
         return jsonify(
@@ -31,3 +42,12 @@ def upload_medias():
 
 def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1] in current_app.config['ALLOWED_EXTENSIONS']
+
+def rename_file(file_path):
+
+        name, extension = file.filename.rsplit('.')
+        while True:
+            name += '_'
+            filename = f'{name}.{extension}'
+            if not os.path.exists(os.path.join(user_folder, filename)):
+                break
