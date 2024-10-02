901d6b60067351a4ae6d18778e9bdfadb7b2359e
Druchek_MA <mdruchek@yandex.ru>
2024-10-02 08:42:47 +0300

    updated upload_medias route, allowed_file function and rename_file function

--- blogs_app/routes/medias.py
+++ blogs_app/routes/medias.py
@@ -1,5 +1,5 @@
 import os
-from fileinput import filename
+from datetime import datetime
 
 from flask import Blueprint, request, current_app, jsonify
 from werkzeug.utils import secure_filename
@@ -15,18 +15,16 @@ def upload_medias():
     db = database.get_db()
     api_key = request.headers.get('Api-Key')
     file = request.files['file']
+    filename_full = secure_filename(file.filename)
     if file and allowed_file(file.filename):
-        filename = secure_filename(file.filename)
         static_folder = os.path.join(current_app.instance_path ,current_app.config['UPLOAD_FOLDER'])
         user_folder = os.path.join(static_folder, api_key)
         if not os.path.exists(static_folder):
             os.makedirs(static_folder)
         if not os.path.exists(user_folder):
             os.makedirs(user_folder)
-        file_path = os.path.exists(os.path.join(user_folder, filename))
-        if os.path.exists(file_path):
-
-        file_path = os.path.join(user_folder, filename)
+        filename_full = rename_file(filename_full)
+        file_path = os.path.join(user_folder, filename_full)
         file.save(file_path)
 
         file_obj = models.Media(url=file_path)
@@ -40,14 +38,10 @@ def upload_medias():
         )
 
 
-def allowed_file(filename):
-    return '.' in filename and filename.rsplit('.', 1)[1] in current_app.config['ALLOWED_EXTENSIONS']
+def allowed_file(filename_full):
+    return '.' in filename_full and filename_full.rsplit('.', 1)[1] in current_app.config['ALLOWED_EXTENSIONS']
 
-def rename_file(file_path):
-
-        name, extension = file.filename.rsplit('.')
-        while True:
-            name += '_'
-            filename = f'{name}.{extension}'
-            if not os.path.exists(os.path.join(user_folder, filename)):
-                break
+def rename_file(filename_full):
+    filename, extension = filename_full.split('.')
+    filename = datetime.now().strftime('%Y_%m_%d_%H_%M_%S')
+    return f'{filename}.{extension}'
