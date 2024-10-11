57f064dfb849670002999fcac88c7de2a569cba3
Druchek_MA <mdruchek@yandex.ru>
2024-10-11 15:15:06 +0300

    updated tests/test_app.py

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,3 +1,12 @@
+import pytest
+
+
 def test_app_config(app):
     assert app.config['TESTING']
     assert app.config['DATABASE'] == 'sqlite:///:memory:'
+
+
+@pytest.mark.parametrize('route', ['/api/tweets/', '/api/users/1'])
+def test__get_route_status(web_client, route):
+    result = web_client.get(route)
+    assert result.status_code == 200
