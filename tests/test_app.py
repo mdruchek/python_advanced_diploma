472ef1514c496a357d6634d9a27d36392df14f88
Mikhail Druchek <mdruchek@yandex.ru>
2024-10-20 11:00:39 +0300

    added tests in test_app.py

--- tests/test_app.py
+++ tests/test_app.py
@@ -7,8 +7,8 @@ def test_app_config(app):
 
 
 @pytest.mark.parametrize('route', ['/api/tweets/', '/api/users/1'])
-def test_get_route_status(web_client, route):
-    result = web_client.get(route)
+def test_get_route_status(client, route):
+    result = client.get(route)
     assert result.status_code == 200
 
 
@@ -25,5 +25,41 @@ def test_init_dev_db_command(runner, monkeypatch):
     assert Recorder.called
 
 
-# def test_create_tweet(create_user, web_client):
-#     web_client.post(hea)
+def test_create_tweet(client, request):
+    response = client.post('/api/tweets/', json={
+        'tweet_data': 'Test tweet',
+        'tweet_media_ids': []
+    }, headers={'Api-Key': 'valid_api_key'})
+    assert response.status_code == 404
+
+    request.getfixturevalue('create_test_user')
+
+    response = client.post('/api/tweets/', json={
+        'tweet_data': 'Test tweet',
+        'tweet_media_ids': []
+    }, headers={'Api-Key': 'valid_api_key'})
+
+    assert response.status_code == 200
+    json_data = response.get_json()
+    assert json_data['result'] is True
+
+
+def test_delete_tweet(client, create_test_user):
+    response = client.delete('/api/tweets/1', headers={'Api-Key': 'valid_api_key'})
+    assert response.status_code == 200
+    json_data = response.get_json()
+    assert json_data['result'] is True
+
+
+def test_get_tweets(client, create_test_user):
+    response = client.get('/api/tweets/')
+    assert response.status_code == 200
+    json_data = response.get_json()
+    assert json_data['result'] is True
+
+
+def test_delete_like(client, create_test_user):
+    response = client.delete('/api/tweets/1/like', headers={'Api-Key': 'valid_api_key'})
+    assert response.status_code == 200
+    json_data = response.get_json()
+    assert json_data['result'] is True
