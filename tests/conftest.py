bdb97cec1474006850a657f3154f00097cbc6e82
Mikhail Druchek <mdruchek@yandex.ru>
2024-10-20 10:58:12 +0300

    updated tests/conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -10,23 +10,30 @@ from blogs_app.models import User
 def app():
     _app = create_app(config_app=TestingConfig)
     with _app.app_context():
-        from blogs_app.database import init_db, drop_all_models_from_db
-    init_db()
+        from blogs_app.database import init_db
+        init_db()
 
     yield _app
 
-    drop_all_models_from_db()
+    with _app.app_context():
+        from blogs_app.database import drop_all_models_from_db
+        drop_all_models_from_db()
 
 
 @pytest.fixture
 def db(app):
     with app.app_context():
-        from blogs_app.database import get_db
-        yield get_db()
+        from blogs_app.database import get_session
+
+        yield get_session()
+
+    with app.app_context():
+        from blogs_app.database import close_session
+        close_session()
 
 
 @pytest.fixture
-def web_client(app):
+def client(app):
     client = app.test_client()
     return client
 
@@ -37,7 +44,7 @@ def runner(app):
 
 
 @pytest.fixture
-def create_user(db):
-    user = User(name='test_user', api_key='test_api_key')
-    db.add(user)
+def create_test_user(db):
+    test_user = User(name="test_user", api_key="valid_api_key")
+    db.add(test_user)
     db.commit()
