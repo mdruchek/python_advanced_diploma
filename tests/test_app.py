88f8e87215092241a7840cb5e8b2aa49efb7b641
Mikhail Druchek <mdruchek@yandex.ru>
2024-10-20 23:35:54 +0300

    test_app.py: updated test_create_tweet and test_delete_tweet tests

--- tests/test_app.py
+++ tests/test_app.py
@@ -11,12 +11,6 @@ def test_app_config(app):
     assert app.config['DATABASE'] == 'sqlite:///:memory:'
 
 
-@pytest.mark.parametrize('route', ['/api/tweets/', '/api/users/1'])
-def test_get_route_status(client, route):
-    result = client.get(route)
-    assert result.status_code == 200
-
-
 def test_init_dev_db_command(runner, monkeypatch):
     class Recorder(object):
         called = False
@@ -30,7 +24,7 @@ def test_init_dev_db_command(runner, monkeypatch):
     assert Recorder.called
 
 
-def test_create_tweet(client, request, session):
+def test_create_tweet(client, request, session, user):
     request_url: str = '/api/tweets/'
     request_data = {
         'tweet_data': 'Test tweet',
@@ -38,33 +32,58 @@ def test_create_tweet(client, request, session):
     }
 
     number_of_tweets_in_database = session.execute(select(func.count(Tweet.id))).scalar()
-    request_headers = {'Api-Key': 'valid_api_key'}
-    response = client.post(request_url, json=request_data, headers=request_headers)
-    assert response.status_code == 403
 
-    request.getfixturevalue('create_test_user')
-    response = client.post(request_url, json=request_data, headers=request_headers)
+    response = client.post(request_url, json=request_data, headers={'Api-Key': 'no_valid_api_key'})
+    assert response.status_code == 403
+    json_data = response.get_json()
+    assert json_data['result'] is False
+    assert json_data['error_type'] == 'Forbidden'
+    assert json_data['error_message'] == 'Access is denied. User with api-key no_valid_api_key not found'
+
+    response = client.post(request_url, json=request_data, headers={'Api-Key': 'valid_api_key'})
     assert response.status_code == 200
     json_data = response.get_json()
     assert json_data['result'] is True
     assert json_data['tweet_id'] == number_of_tweets_in_database + 1
 
 
-def test_delete_tweet(client, create_test_user):
+def test_delete_tweet(client, tweet):
+    response = client.delete('/api/tweets/1', headers={'Api-Key': 'no_valid_api_key'})
+    assert response.status_code == 403
+    json_data = response.get_json()
+    assert json_data['result'] is False
+    assert json_data['error_type'] == 'Forbidden'
+    assert json_data['error_message'] == 'Access is denied. User with api-key no_valid_api_key not found'
+
+    no_found_tweet_number = 9999
+    response = client.delete(f'/api/tweets/{no_found_tweet_number}', headers={'Api-Key': 'valid_api_key'})
+    assert response.status_code == 404
+    json_data = response.get_json()
+    assert json_data['result'] is False
+    assert json_data['error_type'] == 'Not found'
+    assert json_data['error_message'] == f'Tweet with id={no_found_tweet_number} not found'
+
     response = client.delete('/api/tweets/1', headers={'Api-Key': 'valid_api_key'})
+    assert response.status_code == 403
+    json_data = response.get_json()
+    assert json_data['result'] is False
+    assert json_data['error_type'] == 'Forbidden'
+    assert json_data['error_message'] == 'User can only delete their own blogs'
+
+    response = client.delete(f'/api/tweets/{tweet.id}', headers={'Api-Key': 'valid_api_key'})
     assert response.status_code == 200
     json_data = response.get_json()
     assert json_data['result'] is True
 
 
-def test_get_tweets(client, create_test_user):
+def test_get_tweets(client, user):
     response = client.get('/api/tweets/')
     assert response.status_code == 200
     json_data = response.get_json()
     assert json_data['result'] is True
 
 
-def test_delete_like(client, create_test_user):
+def test_delete_like(client, user):
     response = client.delete('/api/tweets/1/like', headers={'Api-Key': 'valid_api_key'})
     assert response.status_code == 200
     json_data = response.get_json()
