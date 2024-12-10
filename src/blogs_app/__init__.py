41ec3489609ac350491e3983ee09d7e37e3c8138
Druchek_MA <mdruchek@yandex.ru>
2024-12-10 11:02:46 +0300

    blogs_app/__init__.py: edited to take into account errors flake8

--- src/blogs_app/__init__.py
+++ src/blogs_app/__init__.py
@@ -1,109 +1,109 @@
+"""blogs_app."""
+
 import os
 
-# from apispec_webframeworks.flask import FlaskPlugin
-# from apispec.ext.marshmallow import MarshmallowPlugin
-from flask import Flask, render_template, send_from_directory
-from flasgger import APISpec, Swagger
+from flasgger import Swagger
+from flask import Flask, Response, render_template, send_from_directory
 
-from .config import ProductionConfig, DevelopmentConfig, TestingConfig
-# from .schemas import UserSchema
+from blogs_app.config import DevelopmentConfig
 
 APP_PATH: str = os.path.dirname(os.path.abspath(__file__))
 TEMPLATE_PATH: str = os.path.join(APP_PATH, 'static')
 
 
 def create_app(config_app=DevelopmentConfig):
-    """
-    Функция создания и настройки экземпляра приложения Flask
+    """Функция создания и настройки экземпляра приложения Flask.
 
-    :return app: экземпляр приложения
-    :rtype app: Flask
-    """
+    Parameters:
+        config_app: конфигурация приложения
 
+    Returns:
+        app: экземпляр приложения
+    """
     app: Flask = Flask(
         __name__,
         instance_path=os.path.join(APP_PATH, '..', '..', 'instance'),
         instance_relative_config=True,
-        template_folder=TEMPLATE_PATH
+        template_folder=TEMPLATE_PATH,
     )
 
     swagger_config = {
-        "headers": [],
-        "openapi": "3.0.1",
-        "components": {
+        'headers': [],
+        'openapi': '3.0.1',
+        'components': {
             'schemas': {
                 'User': {
                     'title': 'User',
                     'type': 'object',
                     'properties': {
                         'id': {
-                            'type': 'integer'
+                            'type': 'integer',
                         },
                         'name': {
                             'type': 'string',
-                            'example': 'Username'
-                        }
-                    }
+                            'example': 'Username',
+                        },
+                    },
                 },
                 'Tweet': {
                     'title': 'Tweet',
                     'type': 'object',
                     'properties': {
                         'id': {
-                            'type': 'integer'
+                            'type': 'integer',
                         },
                         'content': {
                             'type': 'string',
-                            'example': 'Content tweet'
+                            'example': 'Content tweet',
                         },
                         'attachments': {
                             'type': 'array',
                             'items': {
                                 'type': 'string',
-                                'example': '/link/on/media'
-                            }
+                                'example': '/link/on/media',
+                            },
                         },
                         'author': {
-                            '$ref': '#/components/schemas/User'
+                            '$ref': '#/components/schemas/User',
                         },
                         'likes': {
                             'type': 'array',
                             'items': {
                                 'properties': {
                                     'user_id': {
-                                        'type': 'integer'
+                                        'type': 'integer',
                                     },
                                     'name': {
                                         'type': 'string',
-                                        'example': 'Username'
-                                    }
-                                }
-                            }
+                                        'example': 'Username',
+                                    },
+                                },
+                            },
                         },
-                    }
-                }
-            }
+                    },
+                },
+            },
         },
-        "specs": [
+        'specs': [
             {
-                "endpoint": "swagger",
-                "route": "/apidocs/swagger.json",
-                "rule_filter": lambda rule: True,  # all in
-                "model_filter": lambda tag: True,  # all in
-            }
+                'endpoint': 'swagger',
+                'route': '/apidocs/swagger.json',
+                'rule_filter': lambda rule: True,  # all in
+                'model_filter': lambda tag: True,  # all in
+            },
         ],
-        "title": "Tweet App Api",
-        "version": '0.0.1',
-        "termsOfService": "",
-        "static_url_path": "/apidocs/static",
-        "swagger_ui": True,
-        "specs_route": "/apidocs/",
-        "description": "",
+        'title': 'Tweet App Api',
+        'version': '0.0.1',
+        'termsOfService': '',
+        'static_url_path': '/apidocs/static',
+        'swagger_ui': True,
+        'specs_route': '/apidocs/',
+        'description': '',
     }
 
     swagger = Swagger(app, config=swagger_config)
 
-    app.config.from_object(config_app) # конфигурация приложения
+    app.config.from_object(config_app)  # конфигурация приложения
 
     # создание папки приложения
     try:
@@ -113,39 +113,34 @@ def create_app(config_app=DevelopmentConfig):
 
     # подключение модулей
     with app.app_context():
-        from . import factories
-        from . import database
-        from . import models
+        from blogs_app import database, factories, models
         database.init_app(app)
 
     # регистрация blueprint
-    from .routes import tweet, user, medias
+    from blogs_app.routes import medias, tweet, user
     app.register_blueprint(tweet.bp)
     app.register_blueprint(user.bp)
     app.register_blueprint(medias.bp)
 
     @app.route('/')
     def index():
-        """
-        Роут отображения начальной страницы приложения
+        """Роут отображения начальной страницы приложения.
 
-        :return: html страница
-        :rtype: str
+        Returns:
+            html страница
         """
         return render_template('index.html')
 
     @app.route('/<path:relative_link>')
-    def download_file(relative_link: str):
+    def download_file(relative_link: str) -> Response:
+        """Эндпоинт скачивания пользовательских файлов.
+
+        Parameters:
+            relative_link: относительный путь к файлу
+
+        Returns:
+            HTTP ответ содержащий пользовательский файл, либо 'Not found' 404
         """
-        Эндпоинт скачивания пользовательских файлов
-
-        :param relative_link: относительный путь к файлу
-        :type relative_link: str
-
-        :return: HTTP ответ содержащий пользовательский файл, либо 'Not found' 404
-        :rtype: Response
-        """
-
         return send_from_directory(app.instance_path, relative_link)
 
     return app
