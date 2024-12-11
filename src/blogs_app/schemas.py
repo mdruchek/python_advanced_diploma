b590d1d2d15fd506f247bd021f173e23599d3c3d
Druchek_MA <mdruchek@yandex.ru>
2024-12-11 11:57:02 +0300

    deleted schemas.py

--- src/blogs_app/schemas.py
+++
@@ -1,7 +0,0 @@
-from flasgger import Schema, fields
-
-
-class UserSchema(Schema):
-    id = fields.Int(dump_only=True)
-    name = fields.Str(required=True)
-    api_key = fields.Str(required=True)
