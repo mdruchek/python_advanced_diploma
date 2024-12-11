03587336d17e0ee8ceeb4904f0e963d5bdef1aa6
Druchek_MA <mdruchek@yandex.ru>
2024-12-11 11:51:28 +0300

    blogs_app/responses.py: edited to take into account errors flake8

--- src/blogs_app/responses_api.py
+++ src/blogs_app/responses_api.py
@@ -1,77 +1,72 @@
+"""Ответы API."""
+
+
 class ResponsesAPI:
-    """
-    Класс ответов API веб приложения
+    """Класс ответов API веб приложения.
 
     Methods:
         result_true: успешный результат
         error_not_found: ошибка 'объект не найден'
+        error_user_not_found: ошибка 'пользователь с данным api_key не найден'
         error_forbidden: ошибка 'доступ запрещён'
     """
 
     @classmethod
-    def result_true(cls, additional_fields={}):
+    def result_true(cls, additional_fields: set = set) -> dict:
+        """Ответ 'Успешный результат'.
+
+        Parameters:
+            additional_fields: дополнительные поля для включения в возвращаемый словарь
+
+        Returns:
+            Ответ
         """
-        Отвут 'Успешный результат'
-
-        :param additional_fields: дополнительные поля для включения в возвращаемый словарь
-        :type additional_fields: dict
-
-        :return response: ответ
-        :rtype response: dict
-        """
-
         response = {'result': True}
         response.update(additional_fields)
         return response
 
     @classmethod
     def error_not_found(cls, message):
+        """Ответ 'Ошибка, объект не найден'.
+
+        Parameters:
+            message: поясняющее сообщение
+
+        Returns:
+            Ответ
         """
-        Ответ 'Ошибка, объект не найден'
-
-        :param message: поясняющее сообщение
-        :type message: str
-
-        :return: ответ
-        :rtype: dict
-        """
-
         return {
-                'result': False,
-                'error_type': 'Not found',
-                'error_message': message,
+            'result': False,
+            'error_type': 'Not found',
+            'error_message': message,
         }
 
     @classmethod
-    def error_user_not_found(cls, api_key):
+    def error_user_not_found(cls, api_key: str) -> dict:
+        """Ответ 'Ошибка, пользователь с данным api_key не найден'.
+
+        Parameters:
+            api_key: api_key пользователя
+
+        Returns:
+            Ответ
         """
-        Ответ 'Ошибка, пользователь с данным api_key не найден'
-
-        :param api_key: api_key пользователя
-        :type api_key: str
-
-        :return: ответ
-        :rtype: dict
-        """
-
         return {
-                'result': False,
-                'error_type': 'Not found',
-                'error_message': 'Access is denied. User with api-key {api_key} not found'.format(api_key=api_key),
+            'result': False,
+            'error_type': 'Not found',
+            'error_message': 'Access is denied. User with api-key {api_key} not found'.format(api_key=api_key),
         }
 
     @classmethod
-    def error_forbidden(cls, message):
+    def error_forbidden(cls, message: str) -> dict:
+        """Ответ 'Ошибка, доступ запрещён'.
+
+        Parameters:
+            message: поясняющее сообщение
+
+        Returns:
+            Ответ
         """
-        Ответ 'Ошибка, доступ запрещён'
-
-        :param message: поясняющее сообщение
-        :type message: str
-
-        :return: ответ
-        :rtype: dict
-        """
-
         return {
             'result': False,
             'error_type': 'Forbidden',
